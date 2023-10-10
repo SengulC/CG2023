11c6ecffef5a6c9e9364a4742871a7861aef9ff7
MerlinGert <99236983+MerlinGert@users.noreply.github.com>
2023-10-10 19:42:42 +0100

    Week3

--- Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/src/RedNoise.cpp
+++ Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/src/RedNoise.cpp
@@ -4,21 +4,24 @@
 #include <fstream>
 #include <vector>
 #include <glm/glm.hpp>
-
+#include <SDL2/SDL.h>
+#include <CanvasPoint.h>
+#include <Colour.h>
+#include <algorithm>
 
 
 #define WIDTH 320
 #define HEIGHT 240
 
 
-std::vector<float> interpolateSingleFloats(float from, float to, int numberOfValues) {
+/*std::vector<float> interpolateSingleFloats(float from, float to, int numberOfValues) {
     float step = (to - from) / (numberOfValues - 1);
     std::vector<float> values(numberOfValues);
     for (int i = 0; i < numberOfValues; i++) {
         values[i] = from + step * i;
     }
     return values;
-}
+}*/
 
 /*void draw(DrawingWindow &window) {
 	window.clearPixels();
@@ -49,7 +52,7 @@ std::vector<float> interpolateSingleFloats(float from, float to, int numberOfVal
     }
 }*/
 
-std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int numberOfValues) {
+/*std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int numberOfValues) {
     std::vector<glm::vec3> interpolatedValues;
     if (numberOfValues <= 1) return interpolatedValues;  // Edge case
     glm::vec3 step = (to - from) / float(numberOfValues - 1);
@@ -57,8 +60,8 @@ std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 t
         interpolatedValues.push_back(from + step * float(i));
     }
     return interpolatedValues;
-}
-void draw(DrawingWindow &window) {
+}*/
+/*void draw(DrawingWindow &window) {
     window.clearPixels();
     //A good way to tackle this problem is to start by creating variables for the four corners of the window and initialising them with the following primary colours:
     glm::vec3 topLeft(255, 0, 0);        // red
@@ -81,7 +84,7 @@ void draw(DrawingWindow &window) {
             window.setPixelColour(x, y, colour);
         }
     }
-}
+}*/
 
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
@@ -97,28 +100,222 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 }
 
 
-int main(int argc, char *argv[]) {
-    std::vector<float> result;
-    result = interpolateSingleFloats(2.2, 8.5, 7);
-    for(size_t i=0; i<result.size(); i++)
-        std::cout << result[i] << " ";
-    std::cout << std::endl;
+//int main(int argc, char *argv[]) {
+   // std::vector<float> result;
+    // result = interpolateSingleFloats(2.2, 8.5, 7);
+   // for(size_t i=0; i<result.size(); i++)
+   //     std::cout << result[i] << " ";
+    // std::cout << std::endl;
 
-    glm::vec3 from(1.0, 4.0, 9.2);
-    glm::vec3 to(4.0, 1.0, 9.8);
-    std::vector<glm::vec3> results = interpolateThreeElementValues(from, to, 4);
-    for (glm::vec3 vec : results) {
-        std::cout << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")" << std::endl;
+   // glm::vec3 from(1.0, 4.0, 9.2);
+   // glm::vec3 to(4.0, 1.0, 9.8);
+   // std::vector<glm::vec3> results = interpolateThreeElementValues(from, to, 4);
+   // for (glm::vec3 vec : results) {
+    //    std::cout << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")" << std::endl;
+    //  }
+
+
+	//DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
+	//SDL_Event event;
+	//while (true) {
+		// We MUST poll for events - otherwise the window will freeze !
+		//if (window.pollForInputEvents(event)) handleEvent(event, window);
+		//draw(window);
+		// Need to render the frame at the end, or nothing actually gets shown on the screen !
+		//window.renderFrame();
+	//}
+//}
+
+
+//画线
+void drawLine(DrawingWindow &window, const CanvasPoint &p1, const CanvasPoint &p2, const Colour &color) {
+    int dx = p2.x - p1.x;
+    int dy = p2.y - p1.y;
+    int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+    float xInc = dx / (float)steps;
+    float yInc = dy / (float)steps;
+    float x = p1.x;
+    float y = p1.y;
+    for (int i = 0; i <= steps; i++) {
+        window.setPixelColour(x, y, (255 << 24) + (color.red << 16) + (color.green << 8) + color.blue);
+        x += xInc;
+        y += yInc;
+    }
+}
+//画三角
+void drawTriangle(DrawingWindow &window, const CanvasTriangle &triangle, const Colour &color) {
+    drawLine(window, triangle.vertices[0], triangle.vertices[1], color);
+    drawLine(window, triangle.vertices[1], triangle.vertices[2], color);
+    drawLine(window, triangle.vertices[0], triangle.vertices[2], color);
+}
+void fillTriangle(DrawingWindow &window, const CanvasTriangle &triangle, const Colour &color) {
+    std::array<CanvasPoint, 3> sortedVertices = {triangle.vertices[0], triangle.vertices[1], triangle.vertices[2]};
+    std::sort(sortedVertices.begin(), sortedVertices.end(), [](const CanvasPoint &a, const CanvasPoint &b) -> bool {
+        return a.y < b.y;
+    });
+
+    auto computeIntersection = [](const CanvasPoint &a, const CanvasPoint &b, float y) -> float {
+        if (a.y == b.y) return a.x;
+        return a.x + (b.x - a.x) * (y - a.y) / (b.y - a.y);
+    };
+
+    for (int y = static_cast<int>(sortedVertices[0].y); y < static_cast<int>(sortedVertices[1].y); y++) {
+        int startX = computeIntersection(sortedVertices[0], sortedVertices[2], y);
+        int endX = computeIntersection(sortedVertices[0], sortedVertices[1], y);
+        drawLine(window, CanvasPoint(startX, y), CanvasPoint(endX, y), color);
     }
 
-
-	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
-	SDL_Event event;
-	while (true) {
-		// We MUST poll for events - otherwise the window will freeze !
-		if (window.pollForInputEvents(event)) handleEvent(event, window);
-		draw(window);
-		// Need to render the frame at the end, or nothing actually gets shown on the screen !
-		window.renderFrame();
-	}
+    for (int y = static_cast<int>(sortedVertices[1].y); y <= static_cast<int>(sortedVertices[2].y); y++) {
+        int startX = computeIntersection(sortedVertices[0], sortedVertices[2], y);
+        int endX = computeIntersection(sortedVertices[1], sortedVertices[2], y);
+        drawLine(window, CanvasPoint(startX, y), CanvasPoint(endX, y), color);
+    }
 }
+
+int main(int argc, char* argv[]) {
+    DrawingWindow window = DrawingWindow(800, 600, false);
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (window.pollForInputEvents(event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+
+            if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_u) {
+                    // Generate three random vertices
+                    // Inside your event loop
+                    if (event.type == SDL_KEYDOWN) {
+                        if (event.key.keysym.sym == SDLK_u) { // 'u' key pressed
+
+                            // Generate three random vertices
+                            CanvasPoint p1(rand() % window.width, rand() % window.height);
+                            CanvasPoint p2(rand() % window.width, rand() % window.height);
+                            CanvasPoint p3(rand() % window.width, rand() % window.height);
+                            CanvasTriangle triangle(p1, p2, p3);
+
+                            // Generate a random color
+                            Colour randomColor(rand() % 256, rand() % 256, rand() % 256);
+                            fillTriangle(window, triangle, randomColor);  // Fill the triangle with random color
+                            // Draw the triangle
+                            Colour whiteColor(255, 255, 255);
+                            drawTriangle(window, triangle, whiteColor);
+                        }
+                    }
+
+                }
+            }
+        }
+
+        window.renderFrame();
+    }
+
+    return 0;
+}
+//week3-1
+//int main(int argc, char* argv[]) {
+//    DrawingWindow window = DrawingWindow(800, 600, false);
+//    bool running = true;
+//    while (running) {
+//        SDL_Event event;
+//        while (window.pollForInputEvents((event)) ){ // This is correct now
+//            if (event.type == SDL_QUIT) running = false;
+//        }
+//        Colour white(255, 255, 255);  // Define white color
+//        window.clearPixels();
+//        drawLine(window, CanvasPoint(0, 0), CanvasPoint(400, 300), Colour(white));
+//        drawLine(window, CanvasPoint(800, 0), CanvasPoint(400, 300), Colour(white));
+//        drawLine(window, CanvasPoint(400, 0), CanvasPoint(400, 600), Colour(white));
+//        drawLine(window, CanvasPoint(266, 300), CanvasPoint(533, 300), Colour(white));
+//        window.renderFrame();
+//    }
+//}
+//void drawTriangle(DrawingWindow &window, const CanvasTriangle &triangle, const Colour &color) {
+//    drawLine(window, triangle.vertices[0], triangle.vertices[1], color);
+//    drawLine(window, triangle.vertices[1], triangle.vertices[2], color);
+//    drawLine(window, triangle.vertices[0], triangle.vertices[2], color);
+//}
+////
+//
+//
+//
+//void sortVerticesByY(CanvasPoint &p1, CanvasPoint &p2, CanvasPoint &p3) {
+//    if(p1.y > p2.y) std::swap(p1, p2);
+//    if(p1.y > p3.y) std::swap(p1, p3);
+//    if(p2.y > p3.y) std::swap(p2, p3);
+//}
+//
+//int roundToInteger(float value) {
+//    return static_cast<int>(value + 0.5);
+//}
+//
+//void fillScanline(DrawingWindow &window, int y, float startX, float endX, const Colour &color) {
+//    std::cout << "Scanning line at y = " << y << " from x = " << startX << " to x = " << endX
+//              << " with color (" << color.red << ", " << color.green << ", " << color.blue << ")" << std::endl;
+//
+//    int xStart = static_cast<int>(startX);
+//    int xEnd = static_cast<int>(endX);
+//
+//    uint32_t colorValue = (255 << 24) + (color.red << 16) + (color.green << 8) + color.blue;
+//    for(int x = xStart; x <= xEnd; x++) {
+//        window.setPixelColour(x, y, colorValue);
+//    }
+//}
+//
+//
+//void drawFilledTriangle(DrawingWindow &window, const CanvasTriangle &triangle, const Colour &color) {
+//    // Assuming you have a sortVerticesByY function from previous steps
+//    CanvasPoint p1 = triangle.vertices[0];
+//    CanvasPoint p2 = triangle.vertices[1];
+//    CanvasPoint p3 = triangle.vertices[2];
+//    sortVerticesByY(p1, p2, p3);
+//
+//    float slopeA = (p2.y - p1.y) ? (p2.x - p1.x) / (p2.y - p1.y) : 1;
+//    float slopeB = (p3.y - p1.y) ? (p3.x - p1.x) / (p3.y - p1.y) : 1;
+//
+//    // Handle the bottom flat triangle
+//    for(int y = roundToInteger(p1.y); y <= roundToInteger(p2.y); y++) {
+//        float startX = p1.x + slopeA * (y - p1.y);
+//        float endX = p1.x + slopeB * (y - p1.y);
+//        fillScanline(window, y, startX, endX, color);
+//    }
+//
+//    slopeA = (p3.y - p2.y) ? (p3.x - p2.x) / (p3.y - p2.y) : 1;
+//
+//    // Handle the top flat triangle
+//    for(int y = roundToInteger(p2.y); y <= roundToInteger(p3.y); y++) {
+//        float startX = p2.x + slopeA * (y - p2.y);
+//        float endX = p1.x + slopeB * (y - p1.y);
+//        fillScanline(window, y, startX, endX, color);
+//    }
+//    Colour white(255, 255, 255);
+//    drawTriangle(window, triangle, white);
+//}
+//
+//int main(int argc, char* argv[]) {
+//    DrawingWindow window = DrawingWindow(800, 600, false);
+//    bool running = true;
+//
+//    while (running) {
+//        SDL_Event event;
+//        while (window.pollForInputEvents(event)) {
+//            if (event.type == SDL_KEYDOWN) {
+//                if (event.key.keysym.sym == SDLK_u) {
+//
+//                    CanvasPoint p1(rand() % window.width, rand() % window.height);
+//                    CanvasPoint p2(rand() % window.width, rand() % window.height);
+//                    CanvasPoint p3(rand() % window.width, rand() % window.height);
+//                    CanvasTriangle triangle(p1, p2, p3);
+//
+//                    Colour randomColor(rand() % 256, rand() % 256, rand() % 256);
+//                    std::cout << "Generated color: " << randomColor.red << ", " << randomColor.green << ", " << randomColor.blue << std::endl;
+//                    drawFilledTriangle(window, triangle, randomColor);
+//                }
+//            }
+//            window.renderFrame();
+//        }
+//    }
+//    return 0;
+//}
+
